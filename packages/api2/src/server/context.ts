ffe03c07e5dbdeeddb450fdd5b0405e58111d30f
domagoj.vukovic <off.vukovic@gmail.com>
2023-03-03 20:49:09 +0100

    feat: user resolver contract

--- packages/api2/src/server/context.ts
+++ packages/api2/src/server/context.ts
@@ -5,11 +5,6 @@ import type { Context } from '../shared/types'
 export const context = async (): Promise<Context> => {
     return {
         logger,
-        user: {
-            email: '1',
-            firstName: '1',
-            id: '1',
-            lastName: '1',
-        },
+        user: null,
     }
 }
