59561247a43e9a249afd94c9591a44a2341e620f
domagoj.vukovic <off.vukovic@gmail.com>
2023-03-03 20:24:32 +0100

    feat: server setup

---
+++ packages/api2/src/server/type-defs.ts
@@ -0,0 +1,16 @@
+import { join } from 'path'
+
+import { loadFilesSync } from '@graphql-tools/load-files'
+import { mergeTypeDefs } from '@graphql-tools/merge'
+
+export const DEVELOPMENT_SCHEMA_PATH = '../resolvers/**/*.graphql'
+
+const typesDefsArray = loadFilesSync(
+    join(
+        __dirname,
+        DEVELOPMENT_SCHEMA_PATH
+    ),
+    { recursive: true }
+)
+
+export const typeDefs = mergeTypeDefs(typesDefsArray)
