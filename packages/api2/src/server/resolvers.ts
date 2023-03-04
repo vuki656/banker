14b700cd26d4a428e7bf045a74545ea0681142e9
domagoj.vukovic <off.vukovic@gmail.com>
2023-03-04 21:37:22 +0100

    fix: resolver loading/context parsing

--- packages/api2/src/server/resolvers.ts
+++ packages/api2/src/server/resolvers.ts
@@ -2,9 +2,7 @@ import { join } from 'path'
 
 import { loadFilesSync } from '@graphql-tools/load-files'
 import { mergeResolvers } from '@graphql-tools/merge'
-import UserResolver from '../resolvers/user/user.resolver'
 
-// TODO: this isn't loading resolvers correctly. Check the docs, also, generate resolver type seems wrong
 const resolverFiles = loadFilesSync(
     join(
         __dirname,
@@ -13,6 +11,4 @@ const resolverFiles = loadFilesSync(
     { recursive: true }
 )
 
-console.log(resolverFiles)
-
-export const resolvers = mergeResolvers([UserResolver])
+export const resolvers = mergeResolvers(resolverFiles)
