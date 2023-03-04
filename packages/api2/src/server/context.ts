d6d2579168bc5940229b325ffa5c54cab382fb4f
domagoj.vukovic <off.vukovic@gmail.com>
2023-03-04 21:37:22 +0100

    fix: resolver loading/context parsing

--- packages/api2/src/server/context.ts
+++ packages/api2/src/server/context.ts
@@ -1,5 +1,6 @@
 import type { StandaloneServerContextFunctionArgument } from '@apollo/server/dist/esm/standalone'
 import { verify } from 'jsonwebtoken'
+import { User } from '../resolvers/graphql-types.generated'
 
 import env from '../shared/env'
 import { logger } from '../shared/logger'
@@ -8,13 +9,22 @@ import type { Context } from '../shared/types'
 import { cookieValidation } from './context.validation'
 
 // eslint-disable-next-line @typescript-eslint/require-await -- Apollo context has to be async
-export const context = async ({ req, res }: StandaloneServerContextFunctionArgument): Promise<Context> => {
-    // TODO: figure out how to get this from request
-    const cookie = '123'
+export const context = async ({ req }: StandaloneServerContextFunctionArgument): Promise<Context> => {
+    const [,token] = req.headers.authorization?.split(' ') ?? []
 
-    const tokenData = verify(cookie, env.APP_JWT_SECRET)
+    let user: User | null = null
 
-    const { user } = cookieValidation.parse(tokenData)
+    // TODO: without try catch this will fail and prevent graphql apollo playground from working
+    // TODO: Write this so it's not trash
+    try {
+        const tokenData = verify(token ?? '', env.APP_JWT_SECRET)
+
+        const { user: parsedUser } = cookieValidation.parse(tokenData)
+
+        user = parsedUser
+    } catch(error) {
+
+    }
 
     return {
         logger,
