268169d924e2cdd051b500651df5f8dc3fbbc277
domagoj.vukovic <off.vukovic@gmail.com>
2023-03-06 23:38:55 +0100

    fix: prettier formatting for generated files

--- packages/api/codegen.ts
+++ packages/api/codegen.ts
@@ -3,14 +3,14 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 export const SCHEMA_FILES_LOCATION = './src/resolvers/**/*.gql'
 
 const config: CodegenConfig = {
+    hooks: { 
+        afterOneFileWrite: ['prettier --write'] 
+    },
     generates: {
         './src/resolvers/': {
             config: {
                 contextType: '../shared/types#Context',
                 useIndexSignature: true,
-            },
-            hooks: {
-                afterOneFileWrite: 'prettier --write',
             },
             plugins: [
                 'typescript',
