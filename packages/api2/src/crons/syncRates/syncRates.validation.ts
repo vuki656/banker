8d4778b90c50cced3ae3b309612a383cd51ed249
domagoj.vukovic <off.vukovic@gmail.com>
2023-03-06 22:48:22 +0100

    feat: create tranasction mutation

--- packages/api2/src/crons/syncRates/syncRates.validation.ts
+++ packages/api2/src/crons/syncRates/syncRates.validation.ts
@@ -1,9 +1,8 @@
 import { z } from 'zod'
+import { currencyCodeValidation } from '../../resolvers/transaction'
 
 export const ratesValidation = z.object({
-    base: z
-        .string()
-        .length(3),
+    base: currencyCodeValidation,
     disclaimer: z.string(),
     license: z.string(),
     rates: z.record(z.number()),
--- packages/api2/src/resolvers/transaction/transaction.validation.ts
+++ packages/api2/src/resolvers/transaction/transaction.validation.ts
@@ -1,4 +1,22 @@
+import { TransactionStatus } from '@prisma/client'
 import { z } from 'zod'
+
+export const currencyCodeValidation = z
+    .string()
+    .length(3)
+
+export const createTransactionMutationValidation = z.object({
+    amount: z.number(),
+    categoryId: z
+        .string()
+        .uuid()
+        .nullish(),
+    currency: currencyCodeValidation,
+    date: z.date(),
+    description: z.string(),
+    reference: z.string(),
+    status: z.nativeEnum(TransactionStatus)
+})
 
 export const transactionsQueryValidation = z.object({
     categoryId: z
