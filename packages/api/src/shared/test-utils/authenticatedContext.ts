6e321f36067fba6b7ca9d146f6ae8cff3d0c6a53
domagoj.vukovic <off.vukovic@gmail.com>
2023-03-08 01:50:43 +0100

    feat: user tests

---
+++ packages/api/src/shared/test-utils/authenticatedContext.ts
@@ -0,0 +1,23 @@
+import type { ExecuteOperationOptions } from '@apollo/server/dist/esm/externalTypes/graphql'
+import { faker } from '@faker-js/faker'
+
+import type { User } from '../../resolvers/graphql-types.generated'
+import { ContextUser } from '../../server'
+import { logger } from '../logger'
+import type { Context } from '../types'
+
+const user: User = {
+    __typename: 'User',
+    currency: faker.finance.currencyCode(),
+    email: faker.internet.email(),
+    firstName: faker.name.firstName(),
+    id: faker.datatype.uuid(),
+    lastName: faker.name.lastName(),
+}
+
+export const authenticatedContext: ExecuteOperationOptions<Context> = {
+    contextValue: {
+        logger,
+        user: new ContextUser(user),
+    },
+}
--- packages/api/src/shared/utils/validateRequest.ts
+++ packages/api/src/shared/utils/validateRequest.ts
@@ -1,7 +1,9 @@
+import { GraphQLError } from 'graphql'
+
 import type { Context } from '../types'
 
 export const validateRequest = (context: Context) => {
     if (!context.user.value) {
-        throw new Error('Forbidden')
+        throw new GraphQLError('Forbidden')
     }
 }
