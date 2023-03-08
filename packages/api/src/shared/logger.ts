0a4bfea8f4c430e339bcc083df64acccaf69cf2e
domagoj.vukovic <off.vukovic@gmail.com>
2023-03-08 01:54:59 +0100

    feat: redact login password from logs

--- packages/api/src/shared/logger.ts
+++ packages/api/src/shared/logger.ts
@@ -2,8 +2,8 @@ import pino from 'pino'
 
 import env from './env'
 
-// TODO: redact login password
 export const logger = pino({
     enabled: !env.isTest,
     level: env.APP_LOG_LEVEL,
+    redact: ["variables.input.password"]
 })
